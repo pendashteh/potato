18c3ec62bf26f56e0f0c8957ea62eb25ddd4457b
Alexar <github@alexar.me>
2020-02-16 01:43:54 +1100

    Fixes issue: potato does not work if called from PATH

--- lib/potato.inc.sh
+++ lib/potato.inc.sh
@@ -2,22 +2,9 @@
 
 POTATO=1
 
-# Helper functions to get the abolute path for the command
-# Copyright http://stackoverflow.com/a/7400673/257479
-myreadlink() { [ ! -h "$1" ] && echo "$1" || (local link="$(expr "$(command ls -ld -- "$1")" : '.*-> \(.*\)$')"; cd $(dirname $1); myreadlink "$link" | sed "s|^\([^/].*\)\$|$(dirname $1)/\1|"); }
-whereis() { echo $1 | sed "s|^\([^/].*/.*\)|$(pwd)/\1|;s|^\([^/]*\)$|$(which -- $1)|;s|^$|$1|"; }
-whereis_realpath() { local SCRIPT_PATH=$(whereis $1); myreadlink ${SCRIPT_PATH} | sed "s|^\([^/].*\)\$|$(dirname ${SCRIPT_PATH})/\1|"; }
-
 [ ! -z "$POTATO_LOG" ] && __log_path="$POTATO_LOG" || __log_path="/dev/null"
 
 POTATO_COMMAND_DEFAULT='default'
-
-echo $0
-[ -z "$POTATO_PATH" ] &&
-	# This file is being executed directly from command line
-	potato_root=$(dirname $(whereis_realpath "$0")) ||
-		# This file is being sourced from .bashrc
-		potato_root=$POTATO_PATH
 
 function potato_source_all() {
 	local __file_pattern=$1
--- potatorc.sh
+++ potatorc.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Sets potato_root passed by .bashrc
+potato_root=$POTATO_PATH
+
 . $POTATO_PATH/lib/potato.inc.sh
 
 echo "It's a potato farm."
